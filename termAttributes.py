ac7658a27a5fa5abc36687f2923485926da20892
Matt Campbell <mattcampbellav@gmail.com>
2022-01-08 13:34:41 -0500

    20220108_1334

--- termAttributes.py
+++ termAttributes.py
@@ -1,3 +1,4 @@
+import ctypes
 from termcolor import colored
 
 
@@ -5,19 +6,36 @@ class TermAttributes:
 
     # this would be simpler as a function
 
-    info = {'symbol': '[*]', 'color': 'blue'}
-    success = {'symbol': '[+]', 'color': 'green'}
-    warning = {'symbol': '[!]', 'color': 'yellow'}
-    error = {'symbol': '[x]', 'color': 'red'}
+    levels = [
+        {'name': 'info', 'symbol': '[*]', 'color': 'blue'},
+        {'name': 'success', 'symbol': '[+]', 'color': 'green'},
+        {'name': 'warning', 'symbol': '[!]', 'color': 'yellow'},
+        {'name': 'error', 'symbol': '[x]', 'color': 'red'}
+    ]
 
     def __init__(self):
-        pass
+        # enable VT100 support in Windows 10 for colored text in terminal
+        kernel32 = ctypes.WinDLL('kernel32')
+        hStdOut = kernel32.GetStdHandle(-11)
+        mode = ctypes.c_ulong()
+        kernel32.GetConsoleMode(hStdOut, ctypes.byref(mode))
+        mode.value |= 4
+        kernel32.SetConsoleMode(hStdOut, mode)
 
-    @classmethod
     def printText(self, level, text, color=True):
-        for k, v in level.items():
-            if color == True:
-                symbol = colored(f"{level['symbol']}", level['color'])
-            else:
-                symbol = level['symbol']
-        print(f"{symbol} {text}")
+        for l in TermAttributes.levels:
+            if level == l['name']:
+                for k, v in l.items():
+                    if color == True:
+                        symbol = colored(f"{l['symbol']}", l['color'])
+                    else:
+                        symbol = l['symbol']
+                print(f"{symbol} {text}")
+                return
+        for l in TermAttributes.levels:
+            if l['name'] == 'error':
+                symbol = colored(f"{l['symbol']}", l['color'])
+                print(f"{symbol} notice level \'{level}\' not found in function for text \'{text}\'")
+
+
+termAttr = TermAttributes()
